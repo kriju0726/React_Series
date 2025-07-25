1430415f3c4d1ccba9c021f25380eb7c2423a412
kumar.krish07022002@gmail.com <kumar.krish07022003@gmail.com>
2025-07-26 00:16:31 +0530

    Check., "ToggleSwitch" working or not., so link ToggleSwitch in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@
 //import {State} from "./components/hooks/State";
 //import "./components/EV.css";
 //import {LiftingState} from "./components/LiftStateUp";
-//import {ToggleSwitch} from "./components/projects/ToggleSwitch/ToggleSwitch";
+//import {Todo} from "./components/projects/Todo/Todo";
 
 
+import {ToggleSwitch} from "./components/projects/ToggleSwitch/ToggleSwitch";
 
-import {Todo} from "./components/projects/Todo/Todo";
 
 //import {Keys} from "./Keys";
 
@@ -15,7 +15,7 @@ export const App = () => {
   return (
     <section >
      
-      {<Todo />}
+      {<ToggleSwitch />}
       </section>
   );
 };
